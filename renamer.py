cb5e33e6948dba054887b8ef2c872e63b4fa12cc
Шевалдов Станислав <sshevaldov@IBS.RU>
2024-02-28 20:32:45 +0400

    no message

--- renamer.py
+++ renamer.py
@@ -8,4 +8,4 @@ for file in os.listdir():
     source_df = pd.read_csv(file, sep=";").columns
     result = re.findall(r'«.*»', source_df[3])
 
-    os.rename(file, f'{result[0][1:-1]}.csv')
+    os.rename(file, f'{source_df[3].split(",")[2]}{source_df[3].split(",")[1]} {result[0][1:-1]}.csv')
